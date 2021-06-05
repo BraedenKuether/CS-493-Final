6189da4bc1c3b6d89f79bfd009882b56db2379e2
Braeden Kuether <40930466+BraedenKuether@users.noreply.github.com>
2021-06-05 13:51:38 -0700

    Added get by id for playlists

--- api/playlists.js
+++ api/playlists.js
@@ -1,3 +1,32 @@
+/*
+ * API sub-router for playlist collection endpoints.
+ */
 const router = require('express').Router();
 
 const { validateAgainstSchema } = require('../lib/validation');
+const {
+  PlaylistSchema,
+  insertNewPlaylist,
+  getPlaylistById
+} = require('../models/songs');
+
+/*
+ * Route to fetch info about a specific song.
+ */
+router.get('/:id', async (req, res, next) => {
+  try {
+    const playlist = await getPlaylistById(req.params.id);
+    if (playlist) {
+      res.status(200).send(playlist);
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      error: "Unable to fetch song.  Please try again later."
+    });
+  }
+});
+
+module.exports = router;
