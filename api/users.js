63e22d8811c7f71d08a1d9a8d6cb3795835331a0
Braeden Kuether <40930466+BraedenKuether@users.noreply.github.com>
2021-06-05 16:24:26 -0700

    Adding users functionality + login

--- api/users.js
+++ api/users.js
@@ -4,3 +4,88 @@
 const router = require('express').Router();
 
 const { validateAgainstSchema } = require('../lib/validation');
+
+/*
+ * API routes for 'users' collection.
+ */
+
+const router = require('express').Router();
+
+const { validateAgainstSchema } = require('../lib/validation');
+const { generateAuthToken, requireAuthentication } = require('../lib/auth');
+const {
+  UserSchema,
+  insertNewUser,
+  getUserById,
+  validateUser
+} = require('../models/user');
+
+router.post('/', async (req, res) => {
+  if (validateAgainstSchema(req.body, UserSchema)) {
+    try {
+      const id = await insertNewUser(req.body);
+      res.status(201).send({
+        _id: id
+      });
+    } catch (err) {
+      console.error("  -- Error:", err);
+      res.status(500).send({
+        error: "Error inserting new user.  Try again later."
+      });
+    }
+  } else {
+    res.status(400).send({
+      error: "Request body does not contain a valid User."
+    });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  if (req.body && req.body.id && req.body.password) {
+    try {
+      const authenticated = await validateUser(req.body.id, req.body.password);
+      if (authenticated) {
+        res.status(200).send({
+          token: generateAuthToken(req.body.id)
+        });
+      } else {
+        res.status(401).send({
+          error: "Invalid authentication credentials."
+        });
+      }
+    } catch (err) {
+      console.error("  -- error:", err);
+      res.status(500).send({
+        error: "Error logging in.  Try again later."
+      });
+    }
+  } else {
+    res.status(400).send({
+      error: "Request body needs `id` and `password`."
+    });
+  }
+});
+
+router.get('/:id', requireAuthentication, async (req, res, next) => {
+  if (req.user !== req.params.id) {
+    res.status(403).send({
+      error: "Unauthorized to access the specified resource"
+    });
+  } else {
+    try {
+      const user = await getUserById(req.params.id);
+      if (user) {
+        res.status(200).send(user);
+      } else {
+        next();
+      }
+    } catch (err) {
+      console.error("  -- Error:", err);
+      res.status(500).send({
+        error: "Error fetching user.  Try again later."
+      });
+    }
+  }
+});
+
+module.exports = router;
---
+++ models/users.js
@@ -0,0 +1,57 @@
+/*
+ * User schema and data accessor methods.
+ */
+
+const { ObjectId } = require('mongodb');
+const bcrypt = require('bcryptjs');
+
+const { extractValidFields } = require('../lib/validation');
+const { getDBReference } = require('../lib/mongo');
+
+/*
+ * Schema for a User.
+ */
+const UserSchema = {
+  name: { required: true },
+  email: { required: true },
+  password: { required: true }
+};
+exports.UserSchema = UserSchema;
+
+/*
+ * Insert a new User into the DB.
+ */
+exports.insertNewUser = async function (user) {
+  const userToInsert = extractValidFields(user, UserSchema);
+  console.log("  -- userToInsert before hashing:", userToInsert);
+  userToInsert.password = await bcrypt.hash(userToInsert.password, 8);
+  console.log("  -- userToInsert after hashing:", userToInsert);
+  const db = getDBReference();
+  const collection = db.collection('users');
+  const result = await collection.insertOne(userToInsert);
+  return result.insertedId;
+};
+
+/*
+ * Fetch a user from the DB based on user ID.
+ */
+async function getUserById (id, includePassword) {
+  const db = getDBReference();
+  const collection = db.collection('users');
+  if (!ObjectId.isValid(id)) {
+    return null;
+  } else {
+    const projection = includePassword ? {} : { password: 0 }
+    const results = await collection
+      .find({ _id: new ObjectId(id) })
+      .project(projection)
+      .toArray();
+    return results[0];
+  }
+};
+exports.getUserById = getUserById;
+
+exports.validateUser = async function (id, password) {
+  const user = await getUserById(id, true);
+  return user && await bcrypt.compare(password, user.password);
+}
